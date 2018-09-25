bbb797c820eca16f8faf23fb2a340a6aa61dc0fd
Jason <deathenemy666@gmail.com>
2018-09-25 15:50:44 -0500

    formularios

--- scripts/app.js
+++ scripts/app.js
@@ -37,7 +37,7 @@ function suma(numero1,numero2){
 }
 
 var res=suma(88,55);
-console.log(res);*/
+console.log(res);
 
 function iva(numero){
 	var costo=numero*19/100;
@@ -46,3 +46,26 @@ function iva(numero){
 
 var ivar=iva(9000);
 console.log('El iva es →   ', ivar);
+
+var titulos=document.getElementsByTagName('h1');
+console.log(titulos[0])
+
+var clasetexto=document.getElementsByClassName('texto');
+console.log(clasetexto);
+
+var sede= document.getElementById('sede');
+console.log(sede)*/
+
+function iva(precio_producto,porcentaje_iva){
+	var iva_producto=precio_producto*(porcentaje_iva/100);
+	return iva_producto;
+}
+
+function calcular_iva(){
+	var precio=parseInt(document.getElementById("precio").value);
+	var porcentaje_iva=parseInt(document.getElementById("iva").value);
+
+var resultado=iva(precio, porcentaje_iva);
+var salida=document.getElementById('resultado');
+salida.value=resultado;
+}
